3bea6a735d5d08313640d614460c26d87d504c89
Hasan Tareq <hsntareq@gmail.com>
2023-03-06 00:42:29 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from './components/Modal';
+// import Modal from './components/Modal';
+
 
 function App() {
+  const people = [{
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=gxCtHmPOqtg',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=1O0ZOxvvrQ8',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=XVchWB3Bx5k',
+    title: 'web developer',
+    phone: '01626287272'
+  }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App h-screen flex flex-col">
+      
+      <header className='shadow-md py-5 w-full'>
+        <h1 className='font-bold text-2xl'>Karate kata, zuki, uke, geri</h1>
       </header>
+      <main className='flex-1 p-0 container mx-auto p-10'>
+        <ul className='list grid grid-cols-3 cursor-pointer gap-5'>
+          {
+            people.map(person => (
+              <li class="hover:bg-slate-100 shadow-md rounded-md transition border p-5">
+                <Modal name='Hasan Tareq'/>
+                <div className='mt-4'>
+                  <p>{person.phone}</p>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+      </main>
+      <footer className='w-full border-t py-3 bg-gray-200'>
+        Copyright &copy; 2023. All right reserved.
+      </footer>
     </div>
   );
 }
